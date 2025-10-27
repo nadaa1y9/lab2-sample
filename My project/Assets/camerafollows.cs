db44756e49d5473920f596119ab5941dcfe8db68
Nada <899nada@gmail.com>
2025-10-28 00:39:33 +0300

    water_and_bounce

---
+++ My project/Assets/camerafollows.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollows : MonoBehaviour
+{
+    public Transform Target;
+    public float CameraSpeed;
+
+    public float minY,maxY;
+    public float minX,maxX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Target!=null){
+            Vector2 newCamPosition = Vector2.Lerp(transform.position,Target.position,Time.deltaTime*CameraSpeed);
+            float ClampX = Mathf.Clamp(newCamPosition.x, minX, maxX);
+            float ClampY = Mathf.Clamp(newCamPosition.y, minY,maxY);
+
+            transform.position = new Vector3(ClampX, ClampY, -10f);
+        }
+    }
+}
